c27295ebc796e90b4871c7db30b5cf289031b841
utpal2003 <barmanutpal022@gmail.com>
2025-09-06 22:49:29 +0530

    Front url add

--- Backend/server.js
+++ Backend/server.js
@@ -7,7 +7,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({ origin: '*' }));
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: ["Content-Type", "Authorization"]
+}))
+// app.use(cors({ origin: '*' }));
 app.use(express.json());
 
 // Routes
