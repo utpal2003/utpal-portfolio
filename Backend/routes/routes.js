dc8ee95ceccaedb8196dbd7d6c232480f9c3d455
Utpal Barman <136325235+utpal2003@users.noreply.github.com>
2025-11-24 21:31:51 +0530

    heartbeat routes add

--- Backend/routes/routes.js
+++ Backend/routes/routes.js
@@ -7,4 +7,8 @@ router.post('/hireme', hireme);
 router.post('/connect', connect);
 router.post("/chat",handleChat)
 
+router.get("/heartbeat", (req, res) => {
+  res.json({ status: "alive" });
+});
+
 module.exports = router;
