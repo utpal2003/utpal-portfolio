ffa2ea933bdf94ec9849be8fdb4235fc46857b79
Utpal Barman <136325235+utpal2003@users.noreply.github.com>
2025-11-24 22:15:08 +0530

    useeffect problem solved

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,35 +1,35 @@
-
-import './App.css'
-import Navbar from './components/Navbar'
-import Education from './pages/Education'
-import Home from './pages/Home'
-import Projects from './pages/Projects'
-import TechStack from './pages/TechStack'
-import Workxperience from './pages/Workxperience'
-import Contactpage from '../src/components/contact'
-import AnimatedBot from './components/AnimatedBot'
 import { useEffect } from "react";
+import './App.css';
 
-useEffect(() => {
-  fetch(`${import.meta.env.VITE_BACKEND_URL}/api/heartbeat`)
-    .catch(err => console.log("Prewarm failed:", err));
-}, []);
+import Navbar from './components/Navbar';
+import Education from './pages/Education';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import TechStack from './pages/TechStack';
+import Workxperience from './pages/Workxperience';
+import Contactpage from './components/contact';
+import AnimatedBot from './components/AnimatedBot';
 
 function App() {
 
+  // Prewarm backend
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_BACKEND_URL}/api/heartbeat`)
+      .catch(err => console.log("Prewarm failed:", err));
+  }, []);
 
   return (
     <>
-    <AnimatedBot/>   
+      <AnimatedBot />   
       <Navbar />
-      <Home/>
-      <Education/>
-      <TechStack/>
-      <Projects/>
-      <Workxperience/>
-     <Contactpage/>
+      <Home />
+      <Education />
+      <TechStack />
+      <Projects />
+      <Workxperience />
+      <Contactpage />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
